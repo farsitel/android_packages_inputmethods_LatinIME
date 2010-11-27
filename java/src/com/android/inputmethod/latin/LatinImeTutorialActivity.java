6e09f5f91d97957019249e26239e24a98d73e0c6
Reza Mohammadi <reza@farsitel.com>
2010-11-27 21:05:06 +0330

    Actions in the tutorial
    Change-Id: Iaf9afe6e32c90f92142bd50388abc70f6db6384e

--- java/src/com/android/inputmethod/latin/LatinImeTutorialActivity.java
+++ java/src/com/android/inputmethod/latin/LatinImeTutorialActivity.java
@@ -1,45 +1,41 @@
 package com.android.inputmethod.latin;
 
 import android.app.Activity;
-import android.app.Dialog;
-import android.app.ProgressDialog;
 import android.content.Context;
-import android.content.ContentResolver;
-import android.content.DialogInterface;
-import android.content.Intent;
-import android.content.IntentFilter;
-import android.content.SharedPreferences;
-import android.content.pm.PackageManager;
 import android.content.res.Configuration;
 import android.content.res.Resources;
-import android.database.ContentObserver;
 import android.net.Uri;
 import android.os.Bundle;
-import android.os.Handler;
-import android.os.Message;
-import android.os.RecoverySystem;
 import android.view.View;
-import android.view.Window;
-import android.view.WindowManager;
 import android.widget.Button;
-import android.widget.TextView;
-import android.util.Log;
 
-public class LatinImeTutorialActivity extends Activity {
-
-    private final static boolean DEBUG = true;
-    private final static String TAG = "LatinImeTutorialActivity";
+public class LatinImeTutorialActivity extends Activity implements
+        View.OnClickListener {
 
     @Override
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
 
         setContentView(R.layout.ime_tutorial_activity);
+
+        ((Button) findViewById(R.id.next_button)).setOnClickListener(this);
+        ((Button) findViewById(R.id.back_button)).setOnClickListener(this);
     }
 
     @Override
     public void onConfigurationChanged(Configuration newConfig) {
         super.onConfigurationChanged(newConfig);
     }
+
+    public void onClick(View v) {
+        int vid = v.getId();
+        if (vid == R.id.back_button) {
+            setResult(0);
+        } else if (vid == R.id.next_button) {
+            setResult(-1);
+        }
+        finish();
+    }
+
 }
 
